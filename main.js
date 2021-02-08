e06fa68b04148203a49a7999f4f7fd40823ea671
Shiveshsenthilkumar <78719568+Shiveshsenthilkumar@users.noreply.github.com>
2021-02-08 13:11:54 +0530

    Files for my first canvas uploaded

---
+++ main.js
@@ -0,0 +1,49 @@
+
+    var c = document.getElementById("canva");
+    var ctx = c.getContext("2d");
+   ctx.beginPath();
+   ctx.strokeStyle="green";
+   ctx.lineWidth=1;
+   ctx.rect(50,250,700,300)
+    ctx.stroke();
+
+    var c = document.getElementById("canva");
+    var ctx = c.getContext("2d");
+   ctx.beginPath();
+   ctx.strokeStyle="red";
+   ctx.lineWidth=5;
+   ctx.arc(300, 350, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    var c = document.getElementById("canva");
+    var ctx = c.getContext("2d");
+   ctx.beginPath();
+   ctx.strokeStyle="blue";
+   ctx.lineWidth=5;
+   ctx.arc(350, 400, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    
+    var c = document.getElementById("canva");
+    var ctx = c.getContext("2d");
+   ctx.beginPath();
+   ctx.strokeStyle="purple";
+   ctx.lineWidth=5;
+   ctx.arc(400, 350, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    var c = document.getElementById("canva");
+    var ctx = c.getContext("2d");
+   ctx.beginPath();
+   ctx.strokeStyle="blue";
+   ctx.lineWidth=5;
+   ctx.arc(450, 400, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    var c = document.getElementById("canva");
+    var ctx = c.getContext("2d");
+   ctx.beginPath();
+   ctx.strokeStyle="red";
+   ctx.lineWidth=5;
+   ctx.arc(500, 350, 40, 0, 2 * Math.PI);
+    ctx.stroke();
